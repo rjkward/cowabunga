af4ce03d46e3dc4d9f1656c0341152dafbc1d015
Oliver Knight <oliknight95@gmail.com>
2018-08-11 15:00:34 +0100

    When Cow hits edge they fall

--- cowabunga_unity_project/Assets/OK_Stuff/Scripts/OK_CharacterController.cs
+++ cowabunga_unity_project/Assets/OK_Stuff/Scripts/OK_CharacterController.cs
@@ -21,6 +21,7 @@ public class OK_CharacterController : MonoBehaviour {
     float turnInput;
 
     private bool isbounce = false;
+    private bool isfalling = false;
 
     public Quaternion TargetRotation
     {
@@ -31,6 +32,7 @@ public class OK_CharacterController : MonoBehaviour {
     {
         tarRota = transform.rotation;
         PC_Rigid = GetComponent<Rigidbody>();
+        InvokeRepeating("GroundCheck", 1f, 1f);
 
 
         turnInput = 0;
@@ -45,7 +47,11 @@ public class OK_CharacterController : MonoBehaviour {
     private void Update()
     {
         GetInput();
-        Turn();
+        if (isfalling == false)
+        {
+            Turn();
+        }
+
     }
 
     private void FixedUpdate()
@@ -55,15 +61,24 @@ public class OK_CharacterController : MonoBehaviour {
 
     void MoveForward()
     {
-        if(isbounce == true)
+        if (isfalling == false)
         {
-            PC_Rigid.velocity = Vector3.zero;
+            if (isbounce == true)
+            {
+                PC_Rigid.velocity = Vector3.zero;
+            }
+            else
+            {
+                PC_Rigid.velocity = transform.forward * forVel * Time.deltaTime;
+
+            }
         }
         else
         {
-            PC_Rigid.velocity = transform.forward * forVel * Time.deltaTime;
+            transform.rotation = Quaternion.AngleAxis(rotaVel * Time.deltaTime * 100f,Vector3.up);
 
         }
+
     }
 
     void Turn()
@@ -79,4 +94,22 @@ public class OK_CharacterController : MonoBehaviour {
     {
 
     }
+
+    void GroundCheck()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 20f))
+        {
+
+        }
+        else
+        {
+            PC_Rigid.useGravity = true;
+            isfalling = true;
+        }
+
+        if (isfalling == true)
+        transform.localScale += new Vector3(-0.1F, -0.1f, -0.1f);
+    }
 }
+
