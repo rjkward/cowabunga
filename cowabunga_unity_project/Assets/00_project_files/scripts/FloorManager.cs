f5be1e10e32e2cdf3f4b745b84880bb3bce72a91
Rob Ward <rjkward@gmail.com>
2018-08-12 01:06:12 +0100

    better floor maybe

--- cowabunga_unity_project/Assets/00_project_files/scripts/FloorManager.cs
+++ cowabunga_unity_project/Assets/00_project_files/scripts/FloorManager.cs
@@ -4,28 +4,53 @@ using UnityEngine;
 
 public class FloorManager : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _atomPrefab;
-
-    private Rigidbody[][] _floor;
-    private bool[][] _floorModel;
+    private struct IntPair
+    {
+        public readonly int I;
+        public readonly int J;
+        public IntPair(int i, int j)
+        {
+            I = i;
+            J = j;
+        }
+    }
+    
+    private struct BoolPair
+    {
+        public bool Falling;
+        public bool Disabled;
+    }
+    
     private const int AtomsPerSide = 25;
     private const float AtomLength = 2f;
+    private const float EdgeFallChance = 0.05f;
+    private const float InnerFallChance = 0.00005f;
+    private const float DisableDepth = -50f;
+    private readonly Rigidbody[][] _floorView = new Rigidbody[AtomsPerSide][];
+    private readonly BoolPair[][] _floorModel = new BoolPair[AtomsPerSide][];
+    private readonly List<IntPair> _fallBuffer = new List<IntPair>();
+    private readonly WaitForSeconds _wait = new WaitForSeconds(1f / 60f);
+    [SerializeField]
+    private Rigidbody _atomPrefab;
 
     private void Awake()
     {
-        _floor = new Rigidbody[AtomsPerSide][];
-        _floorModel = new bool[AtomsPerSide][];
-        for (int i = 0; i < _floor.Length; i++)
+        for (int i = 0; i < AtomsPerSide; i++)
         {
-            _floor[i] = new Rigidbody[AtomsPerSide];
-            _floorModel[i] = new bool[AtomsPerSide];
+            _floorView[i] = new Rigidbody[AtomsPerSide];
+            _floorModel[i] = new BoolPair[AtomsPerSide];
         }
     }
 
-    private void Start ()
+    private void Start()
     {
         GenerateFloor();
         StartCoroutine(Erode());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
     private void GenerateFloor()
@@ -35,99 +60,101 @@ public class FloorManager : MonoBehaviour
             for (int j = 0; j < AtomsPerSide; j++)
             {
                 Rigidbody newAtom = Instantiate(_atomPrefab, transform);
-                _floor[i][j] = newAtom;
+                _floorView[i][j] = newAtom;
                 newAtom.position = new Vector3(i * AtomLength, 0f, j * AtomLength);
             }
         }
     }
-
-    // private void Cleanup()
-    // {
-    //     StopAllCoroutines();
-    //     for (int i = 0; i < _floor.Count; i++)
-    //     {
-    //         if (_floor[i] != null)
-    //         {
-    //             Destroy(_floor[i].gameObject);
-    //         }
-    //     }
-    //     
-    //     _floorAtoms.Clear();
-    // }
     
-    private readonly WaitForFixedUpdate _waitForFixed = new WaitForFixedUpdate();
-    private readonly WaitForSeconds _waitForOne = new WaitForSeconds(1f);
-    private const float EdgeFallChance = 0.01f;
-    private const float InnerFallChance = 0.0001f;
 
-    private readonly List<Vector2> _fallBuffer = new List<Vector2>();
 
     private IEnumerator Erode()
     {
-        while (true)
+        while (true) // aaaaaagh!!!
         {
+            bool finished = true;
             for (int i = 0; i < AtomsPerSide; i++)
             {
-                bool[] row = _floorModel[i];
+                BoolPair[] row = _floorModel[i];
                 for (int j = 0; j < AtomsPerSide; j++)
                 {
-                    bool eroded = row[j];
-                    if (eroded)
+                    BoolPair atomState = row[j];
+                    if (atomState.Falling)
                     {
+                        if (!atomState.Disabled)
+                        {
+                            finished = false;
+                            Rigidbody atom = _floorView[i][j];
+                            if (atom.transform.position.y < DisableDepth)
+                            {
+                                atom.gameObject.SetActive(false);
+                                atomState.Disabled = true;
+                            }
+                        }
+                        
                         continue;
                     }
-
-                    int openEdges = GetOpenEdges(i, j);
-                    if (openEdges > 0 &&
-                        (openEdges > 2 || Random.value * openEdges < EdgeFallChance))
+                    
+                    finished = false;
+                    int fallenNeighbours = GetFallenNeighbourCount(i, j);
+                    if (fallenNeighbours > 0 &&
+                        (fallenNeighbours > 2 || Random.value * fallenNeighbours < EdgeFallChance))
                     {
-                        _fallBuffer.Add(new Vector2(i, j));
+                        _fallBuffer.Add(new IntPair(i, j));
                         continue;
                     }
 
                     if (Random.value < InnerFallChance)
                     {
-                        _fallBuffer.Add(new Vector2(i, j));
+                        _fallBuffer.Add(new IntPair(i, j));
                     }
                 }
-                yield return _waitForFixed;
+                
+                yield return _wait;
+            }
+
+            int count = _fallBuffer.Count;
+            for (int x = 0; x < count; x++)
+            {
+                IntPair atomPos = _fallBuffer[x];
+                int i = atomPos.I;
+                int j = atomPos.J;
+                _floorView[i][j].isKinematic = false;
+                _floorModel[i][j].Falling = true;
             }
             
-            _fallBuffer.ForEach(vector2 =>
-            {
-                int i = (int)vector2.x;
-                int j = (int)vector2.y;
-                _floor[i][j].isKinematic = false;
-                _floorModel[i][j] = true;
-            });
-            
             _fallBuffer.Clear();
+            if (finished)
+            {
+                Debug.LogError("done");
+                yield break;
+            }
         }
     }
 
-    private int GetOpenEdges(int i, int j)
+    private int GetFallenNeighbourCount(int i, int j)
     {
-        int openSides = 0;
-        if (j + 1 == AtomsPerSide || _floorModel[i][j + 1])
+        int fallen = 0;
+        if (j + 1 == AtomsPerSide || _floorModel[i][j + 1].Falling)
         {
-            openSides += 1;
+            fallen += 1;
         }
         
-        if (j == 0 || _floorModel[i][j - 1])
+        if (j == 0 || _floorModel[i][j - 1].Falling)
         {
-            openSides += 1;
+            fallen += 1;
         }
         
-        if (i + 1 == AtomsPerSide || _floorModel[i + 1][j])
+        if (i + 1 == AtomsPerSide || _floorModel[i + 1][j].Falling)
         {
-            openSides += 1;
+            fallen += 1;
         }
         
-        if (i == 0 || _floorModel[i - 1][j])
+        if (i == 0 || _floorModel[i - 1][j].Falling)
         {
-            openSides += 1;
+            fallen += 1;
         }
 
-        return openSides;
+        return fallen;
     }
 }
