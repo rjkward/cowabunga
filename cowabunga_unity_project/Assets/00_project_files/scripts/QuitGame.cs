db7f51de46b4e798708d2da58906f8198482b2d8
Rob Ward <rjkward@gmail.com>
2020-01-21 18:39:09 +0000

    quit game

---
+++ cowabunga_unity_project/Assets/00_project_files/scripts/QuitGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool quit = Input.GetKey(KeyCode.Escape);
+        if (quit)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
